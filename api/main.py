06976ef3eb965b75ae77f79016735055e06534ab
Julia <jule.niederrossla@gmx.de>
2025-07-31 13:14:02 +0200

    refactor(main): remove unused code

--- api/main.py
+++ api/main.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 from fastapi import FastAPI
 from router import router
 
@@ -7,12 +5,6 @@ app = FastAPI()
 
 app.include_router(router) 
 
-
 @app.get("/")
 def read_root():
     return {"Wellcome to": "Short Location"}
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
