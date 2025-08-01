566206d4b3b07e2f0ca0dc633d79bba7c65f4c4b
Julia <jule.niederrossla@gmx.de>
2025-08-01 08:53:22 +0200

    refactor(database): change parameter name datadabe to session

--- api/router.py
+++ api/router.py
@@ -4,7 +4,7 @@ from geocoding import (
     PhotonCityLocation
 )
 from sqlalchemy.orm import Session
-from database import get_database, add_city_to_database
+from database import get_session, add_city_to_database
 from database_model import City
 from sqlalchemy.exc import OperationalError
 
@@ -28,29 +28,28 @@ def short_location(
     # * add pattern to check for digits in names
     # * add checking if city string is a real city and not a village or region
     # * add sth when one city name returns multiple results
-    # * change get_geo_data to servic class
        
     city_geo_data = geo_locator.get_geo_data(city_name)
     
     if not city_geo_data:
         raise HTTPException(status_code=500, detail=f"Cannot find city: {city_name}")
     try:
-        add_city_to_database(city_geo_data, database)
+        add_city_to_database(city_geo_data, session)
     except OperationalError:
         raise HTTPException(status_code=503, detail="Cannot connect to database")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Unknown error. {e}")
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {e}")
 
     my_city_url = CityURL(short_url=f"http://localhost:8000/{city_geo_data.id}")
 
     return my_city_url
 
 @router.get("/{short}", response_model=CityCoordinate, tags=["city"])
-def get_city_from_short(short:str, database:Session = Depends(get_database)) -> CityCoordinate:
-    data = database.query(City).filter(City.id == short).first()
+def get_city_from_short(short:str, session:Session = Depends(get_session)) -> CityCoordinate:
+    data = session.query(City).filter(City.id == short).first()
     
     if not data:
-        raise HTTPException(status_code=404, detail=f"Short URL not found")
+        raise HTTPException(status_code=404, detail=f"Short url not found")
     
     city_coords  = CityCoordinate(latitude=data.lat, longitude=data.lng)
     return city_coords
